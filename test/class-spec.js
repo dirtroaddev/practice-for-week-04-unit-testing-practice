3d0aa5a903d7b4b1ffbe0ddc9439fc79023fa4e3
dirtroaddev <rongoodwindev@gmail.com>
2024-02-15 16:31:28 -0500

    Finished creating the tests

--- test/class-spec.js
+++ test/class-spec.js
@@ -3,32 +3,39 @@ const expect = chai.expect;
 
 const { Word } = require("../class");
 
+
 describe("Word", function () {
+  let myWord;
+  beforeEach(() => {
+    myWord = new Word("test");
+  });
+
   describe("Word constructor function", function () {
+
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(myWord).to.have.property('word');
     });
   
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(myWord.word).to.equal('test')
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(myWord.removeVowels()).to.equal('tst');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(myWord.removeConsonants()).to.equal('e');
     });
   });
   
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(myWord.pigLatin()).to.equal('esttay')
     });
   });
 });
