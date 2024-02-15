9242a74f9f5f5f881e90545b8c725797767a4040
dirtroaddev <rongoodwindev@gmail.com>
2024-02-15 15:57:27 -0500

    Just building the functions

--- funcs.js
+++ funcs.js
@@ -1,13 +1,27 @@
 function isFive(num) {
   // Your code here
+  return num === 5;
 }
 
 function isOdd(number) {
   // Your code here
+    if(typeof number == "number") {
+      return number % 2 !== 0;
+    }
+
+    throw Error();
+
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  const arr = [];
+
+  for (let i = min; i <= max; i += step) {
+    arr.push(i);
+  }
+
+  return arr;
 }
 
 
